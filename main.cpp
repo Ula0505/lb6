05bfd816190dca12c361ac6b1508ef6d5f0d38d3
Ula0505 <ulana.duenkova@gmail.com>
2021-05-06 12:55:00 +0300

    5

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <stdio.h>
+using namespace std;
 int main() {
+	setlocale(LC_ALL, "Russian");
+	int furstNum, secondNum, Sum;
+	cout << "¬ведите первое число: ";
+	cin >> firstNum;
+	cout << "¬ведите второе число: ";
+	cin >> secondNum;
 	return 0;
 }
